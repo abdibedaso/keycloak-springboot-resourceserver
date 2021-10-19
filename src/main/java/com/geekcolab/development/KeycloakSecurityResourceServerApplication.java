7ba8b63998e462b8094d1e66dfaa882e5424412a
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-19 01:50:18 -0500

    init

---
+++ src/main/java/com/geekcolab/development/KeycloakSecurityResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.geekcolab.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSecurityResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSecurityResourceServerApplication.class, args);
+	}
+}
