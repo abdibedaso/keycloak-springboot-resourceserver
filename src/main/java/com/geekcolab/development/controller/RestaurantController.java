1136e5d8d391ead0173324f9f845c3b3f41b8454
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-19 22:47:36 -0500

    EnableIstio in RestaurantController

--- src/main/java/com/geekcolab/development/controller/RestaurantController.java
+++ src/main/java/com/geekcolab/development/controller/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.geekcolab.development.controller;
 
+import com.github.piomin.springboot.istio.annotation.EnableIstio;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/restaurant")
+@EnableIstio(version = "v1", timeout = 3, numberOfRetries = 3)
 public class RestaurantController {
 
-	@PreAuthorize("hasRole('RESTURANT')")
+	@PreAuthorize("hasRole('RESTAURANT')")
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ResponseEntity<?> sayHello() {
 
-		return new ResponseEntity<>("Hi! RESTURANT, you are authorized to view this response!", HttpStatus.OK);
+		return new ResponseEntity<>("Hi! RESTAURANT, you are authorized to view this response!", HttpStatus.OK);
 
 	}
 
